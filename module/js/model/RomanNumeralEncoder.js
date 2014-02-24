493ab02af4a4284b88da207af15cc4978d45b60c
jameslove <hello@jameslove.co.uk>
2014-02-24 21:55:32 +0000

    Initial Commit

---
+++ module/js/model/RomanNumeralEncoder.js
@@ -0,0 +1,34 @@
+// Define the module.
+define(
+    [
+        /* No dependencies. */
+    ],
+    function(){
+
+
+        // Constructor
+
+        function Validation(){
+            // ...
+        }
+
+        // Define the class methods.
+        Validation.prototype = {
+
+
+
+
+
+        };
+
+
+        // -------------------------------------------------- //
+        // -------------------------------------------------- //
+
+
+        // Return the module constructor.
+        return( Validation );
+
+
+    }
+);
